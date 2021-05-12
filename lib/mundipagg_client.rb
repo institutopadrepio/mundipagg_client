232f8547b47706ffccd146c1c6c3a54172c84fe4
Anchieta Júnior <santosjr87@gmail.com>
2021-05-12 00:12:10 -0300

    Update client

--- lib/mundipagg_client.rb
+++ lib/mundipagg_client.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 require "active_interaction"
+require "mundipagg_client/mundipagg_base"
+require "mundipagg_client/configuration"
+require "mundipagg_client/application_formatter"
+require "mundipagg_client/operations/customers/create"
 require_relative "mundipagg_client/version"
 
 module MundipaggClient
-  class Error < StandardError; end
-  # Your code goes here...
+  module_function
+
+  class MundipaggClientConfiguration
+    def self.configuration
+      @configuration ||= Configuration.new
+    end
+
+    def self.configure(&block)
+      yeld(configuration)
+    end
+  end
 end
