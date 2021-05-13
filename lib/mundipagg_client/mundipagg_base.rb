59c6ed26dc2f456fbac444841b8efa9dd7f8e55c
Anchieta Júnior <santosjr87@gmail.com>
2021-05-13 12:45:36 -0300

    Refactor connection

--- lib/mundipagg_client/mundipagg_base.rb
+++ lib/mundipagg_client/mundipagg_base.rb
@@ -3,21 +3,23 @@
 module MundipaggClient
   class MundipaggBase < ActiveInteraction::Base
     API_VERSION = "1"
-    BASE_URL = "https://api.mundipagg.com/core/v#{API_VERSION}/"
+    BASE_URL = "https://api.mundipagg.com/core/v#{API_VERSION}"
 
     def headers
       {
         "Content-Type" => "application/json",
-        "Accept" => "application/json",
-        "Authorization" => "Basic #{MundipaggClient::MundipaggClientConfiguration.configuration.api_key}}"
+        "Accept" => "application/json"
       }
     end
 
+    def build_connection
+      connection = Faraday.new(headers: headers)
+      connection.basic_auth(MundipaggClient::MundipaggClientConfiguration.configuration.api_key, '')
+      connection
+    end
+
     def connection
-      @connection ||= Faraday.new(
-        url: "https://api.mundipagg.com/",
-        headers: headers
-      )
+      @connection ||= build_connection
     end
   end
 end
