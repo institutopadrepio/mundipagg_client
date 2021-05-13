f684c37bd7163647a5778e46b1d0f46643bb8537
Anchieta Júnior <santosjr87@gmail.com>
2021-05-13 12:46:13 -0300

    Update create customer operation

--- lib/mundipagg_client/operations/customers/create.rb
+++ lib/mundipagg_client/operations/customers/create.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+require "pry"
+
 module MundipaggClient
   module Operations
     module Customers
       class Create < MundipaggClient::MundipaggBase
         hash :params do
-          string :amount
-          string :customer_id
+          string :name
+          string :email
+          string :document
         end
 
         def execute
@@ -18,50 +21,22 @@ module MundipaggClient
         private
 
         def request
-          @request ||= connection.post(
-            path: "/charges",
-            headers: headers,
-            body: customer_params.to_json
-          )
+          @request ||= connection.post("#{BASE_URL}/customers") do |req|
+            req.body = customer_params.to_json
+          end
         end
 
         def customer_params
-          {}.tap do |hash|
-            hash[:amount] = params[:amount]
-            hash[:customer_id] = params[:customer_id]
-            payment_params(hash)
-            credit_card_params(hash)
-          end
-        end
-
-        def payment_params(hash)
-          hash[:payment] = {
-            payment_method: "credit_card",
-            credit_card: {
-              capture: true,
-              recurrence: true,
-              installments: installments,
-              statement_descriptor: "Padre Paulo Ricardo"
-            }
+          {
+            name: params[:name],
+            email: params[:email],
+            type: 'individual',
+            document: formatted_document
           }
         end
 
-        def credit_card_params(hash)
-          if params[:card_id].present?
-            hash[:payment][:credit_card][:card_id] = params[:card_id]
-          else
-            hash[:payment][:credit_card][:card] = {
-              number: params[:credit_card_number],
-              holder_name: params[:credit_card_holder_name],
-              exp_month: params[:credit_card_exp_month],
-              exp_year: params[:credit_card_exp_year],
-              cvv: params[:credit_card_cvv]
-            }
-          end
-        end
-
-        def installments
-          params[:installments] || 1
+        def formatted_document
+          params[:document].present? ? params[:document].gsub(".", "").gsub("-", "") : nil
         end
       end
     end
