3b64aeb93fc80078cbaf9b5d15cb3d8614e1ac3d
Anchieta Júnior <santosjr87@gmail.com>
2021-05-17 14:48:22 -0300

    Add credit card delete operation

---
+++ lib/mundipagg_client/operations/credit_cards/delete.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "pry"
+
+module MundipaggClient
+  module Operations
+    module CreditCards
+      class Delete < MundipaggClient::MundipaggBase
+        string :customer_id
+        string :card_id
+
+        def execute
+          raise "Invalid Mundipagg operation" unless request.success?
+
+          JSON.parse(request.body)
+        end
+
+        private
+
+        def request
+          @request ||= connection.delete("#{BASE_URL}/customers/#{customer_id}/cards/#{card_id}")
+        end
+      end
+    end
+  end
+end
