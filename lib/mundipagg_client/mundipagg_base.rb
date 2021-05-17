3a85942b5f952fc259c3199a5dab6651351610f3
Anchieta Júnior <santosjr87@gmail.com>
2021-05-17 11:12:26 -0300

    Update base and create operations

--- lib/mundipagg_client/mundipagg_base.rb
+++ lib/mundipagg_client/mundipagg_base.rb
@@ -21,5 +21,26 @@ module MundipaggClient
     def connection
       @connection ||= build_connection
     end
+
+    def sanitize_numbers(number)
+      number.gsub(/[^\d]/, "")
+    end
+
+    def sanitize_names(name)
+      name = name.gsub(/(á|à|ã|â|ä)/, "a")
+                 .gsub(/(é|è|ê|ë)/, "e")
+                 .gsub(/(í|ì|î|ï)/, "i")
+                 .gsub(/(ó|ò|õ|ô|ö)/, "o")
+                 .gsub(/(ú|ù|û|ü)/, "u")
+      name = name.gsub(/(Á|À|Ã|Â|Ä)/, "A")
+                 .gsub(/(É|È|Ê|Ë)/, "E")
+                 .gsub(/(Í|Ì|Î|Ï)/, "I")
+                 .gsub(/(Ó|Ò|Õ|Ô|Ö)/, "O")
+                 .gsub(/(Ú|Ù|Û|Ü)/, "U")
+      name = name.gsub(/ñ/, "n").gsub(/Ñ/, "N")
+      name = name.gsub(/ç/, "c").gsub(/Ç/, "C")
+      name = name.gsub(/[^A-Z a-z]/, "")
+      name.upcase
+    end
   end
 end
--- lib/mundipagg_client/operations/credit_cards/create.rb
+++ lib/mundipagg_client/operations/credit_cards/create.rb
@@ -40,27 +40,6 @@ module MundipaggClient
             holder_document: sanitize_numbers(params[:holder_document])
           }
         end
-
-        def sanitize_numbers(number)
-          number.gsub(/[^\d]/, "")
-        end
-
-        def sanitize_names(name)
-          name = name.gsub(/(á|à|ã|â|ä)/, "a")
-                     .gsub(/(é|è|ê|ë)/, "e")
-                     .gsub(/(í|ì|î|ï)/, "i")
-                     .gsub(/(ó|ò|õ|ô|ö)/, "o")
-                     .gsub(/(ú|ù|û|ü)/, "u")
-          name = name.gsub(/(Á|À|Ã|Â|Ä)/, "A")
-                     .gsub(/(É|È|Ê|Ë)/, "E")
-                     .gsub(/(Í|Ì|Î|Ï)/, "I")
-                     .gsub(/(Ó|Ò|Õ|Ô|Ö)/, "O")
-                     .gsub(/(Ú|Ù|Û|Ü)/, "U")
-          name = name.gsub(/ñ/, "n").gsub(/Ñ/, "N")
-          name = name.gsub(/ç/, "c").gsub(/Ç/, "C")
-          name = name.gsub(/[^A-Z a-z]/, "")
-          name.upcase
-        end
       end
     end
   end
